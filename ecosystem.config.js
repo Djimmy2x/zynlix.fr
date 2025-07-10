b969eab6065db01068e3f9e521db1cb730ffc8de
Djimmy2x <khati.djamel.1995@gmail.com>
2025-07-11 00:04:11 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,8 @@
 export default {
   apps: [{
     name: 'zynlix-app',
-    script: 'dist/index.js',
+    script: './dist/index.js',
     cwd: '/home/zynlix/zynlix',
-    user: 'zynlix',
     instances: 1,
     autorestart: true,
     watch: false,
@@ -11,10 +10,6 @@ export default {
     env: {
       NODE_ENV: 'production',
       PORT: 3000
-    },
-    error_file: '/home/zynlix/zynlix/logs/err.log',
-    out_file: '/home/zynlix/zynlix/logs/out.log',
-    log_file: '/home/zynlix/zynlix/logs/combined.log',
-    time: true
+    }
   }]
 }; 
