bd1f13b304a559e215689360d3a0a371b2b938db
Djimmy2x <khati.djamel.1995@gmail.com>
2025-07-11 01:57:24 +0200

    v7

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,28 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { ThemeProvider } from "@/components/theme-provider";
-import Home from "@/pages/home";
-import Admin from "@/pages/admin";
-import Services from "@/pages/services";
-import Company from "@/pages/company";
-import Support from "@/pages/support";
-import NotFound from "@/pages/not-found";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/admin" component={Admin} />
-      <Route path="/services/*" component={Services} />
-      <Route path="/company/*" component={Company} />
-      <Route path="/support/*" component={Support} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
+import { Route, Switch } from 'wouter'
+import Navigation from './components/navigation'
+import Footer from './components/footer'
+import Home from './pages/home'
+import Services from './pages/services'
+import Company from './pages/company'
+import Support from './pages/support'
+import NotFound from './pages/not-found'
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="light">
-        <TooltipProvider>
-          <Toaster />
-          <Router />
-        </TooltipProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
-  );
+    <div className="min-h-screen bg-background">
+      <Navigation />
+      <main>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/company" component={Company} />
+          <Route path="/support" component={Support} />
+          <Route component={NotFound} />
+        </Switch>
+      </main>
+      <Footer />
+    </div>
+  )
 }
 
-export default App;
+export default App
